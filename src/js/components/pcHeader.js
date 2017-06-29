7b27b9d82cb44d493566aa19d255177d9b1eb213
smile_ucas <xiaoxiaotvb@126.com>
2017-06-29 16:43:10 +0800

    init

---
+++ src/js/components/pcHeader.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+import {Menu, Icon} from 'antd';
+import '../../css/pc.css'
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+export default class PCHeader extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            current: 'top'
+        };
+    }
+    render() {
+        return (
+            <header>
+                <Row>
+                    {/*?一行有24格?*/}
+
+                    {/*占两格左留白*/}
+                    <Col span={2}></Col>
+
+                    <Col span={4}>
+                        <a href="/" class="logo">
+                            <img src="./src/images/logo.png" alt="logo"/>
+                            <span>ReactNews</span>
+                        </a>
+                    </Col>
+                    <Col span={16}>
+                        {/*mode="horizontal"水平排列*/}
+                        {/*selectedKeys={[this.state.current]}，，默认选中的 ,值初始化成了Menu.Item key="top" 的值，在上面的constructor()中 */}
+                        <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+                            <Menu.Item key="top">
+                                {/*Icon type="appstore"  设置哪种类型的小图标*/}
+                                <Icon type="appstore"/>头条
+                            </Menu.Item>
+                            <Menu.Item key="shehui">
+                                <Icon type="appstore"/>社会
+                            </Menu.Item>
+                            <Menu.Item key="guonei">
+                                <Icon type="appstore"/>国内
+                            </Menu.Item>
+                            <Menu.Item key="guoji">
+                                <Icon type="appstore"/>国际
+                            </Menu.Item>
+                            <Menu.Item key="yule">
+                                <Icon type="appstore"/>娱乐
+                            </Menu.Item>
+                            <Menu.Item key="tiyu">
+                                <Icon type="appstore"/>体育
+                            </Menu.Item>
+                            <Menu.Item key="keji">
+                                <Icon type="appstore"/>科技
+                            </Menu.Item>
+                            <Menu.Item key="shishang">
+                                <Icon type="appstore"/>时尚
+                            </Menu.Item>
+                        </Menu>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+            </header>
+        );
+    };
+}
---
+++ src/js/components/pcIndex.js
@@ -0,0 +1,17 @@
+/**
+ * Created by xiaoxiao on 2017/6/29.
+ */
+import React from 'react';
+import PCHeader from './pcHeader';
+export default class PCIndex extends React.Component {
+    render() {
+        return (
+            <div>
+                <PCHeader></PCHeader>
+            </div>
+        );
+    };
+}
+
+
+
