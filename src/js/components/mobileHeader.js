7e4bac17b4d061a45a4bc4a5731569683562de70
smile_ucas <xiaoxiaotvb@126.com>
2017-06-30 11:22:11 +0800

    pc_register

--- src/js/components/mobileHeader.js
+++ src/js/components/mobileHeader.js
@@ -8,6 +8,7 @@ import {Row,Col} from 'antd'
 
 export default class MobileHeader extends React.Component{
     render(){
+        
         return (
             <div id="mobileheader">
                 <header>
--- src/js/components/pcHeader.js
+++ src/js/components/pcHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Row, Col} from 'antd';
 import {
     Menu, Icon,
-    Tabs, meaaage, Form,Modal, Input, Button, Checkbox
+    Tabs, message, Form, Modal, Input, Button, Checkbox
 } from 'antd';
 const FormItem = Form.Item;
 const TabPane = Tabs.TabPane;
@@ -17,31 +17,54 @@ class PCHeader extends React.Component {
 
 
             //模态框显示初始化false   用户昵称userNickName初始化空    userid当前还没有，初始化为0
-            modalVisible:false,
-            action:'login',
-            hasLogined:false,
-            userNickName:'',
-            userid:0
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
         };
     }
 
-    setModalVisible(value){
-        this.setState({modalVisible:value})
+    setModalVisible(value) {
+        this.setState({modalVisible: value})
     };
-    handleClick(e){
-        if(e.key="register"){
-            this.setState({current:'register'})
+
+    handleClick(e) {
+        if (e.key = "register") {
+            this.setState({current: 'register'})
 
             //调用setModalVisible方法，让模态框显示
             this.setModalVisible(true);
         }
         else {
             //点击哪一个就让哪个高亮
-            this.setState({current:'e.key'})
+            this.setState({current: 'e.key'})
         }
     };
-    handleSubmit(e){
-    //    页面开始向API进行提交
+
+    handleSubmit(e) {
+        //    页面开始向API进行提交
+        e.preventDefault();//阻止冒泡
+
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData= this.props.form.getFieldsValue();
+        console.log(formData);//是个对象，把模态框中账号、密码、确认密码获取到
+
+
+        //采用fetch（fetch.then.then）     postman是chrome的插件。进行接口的测试
+        //以下的参数中username=userName是登陆用的
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="+formData.r_userName+"&r_password="+formData.r_password+"&r_confirmPassword="+formData.r_confirmPassword,myFetchOptions).
+        then(response=>response.json()).then(json=>{
+            console.log(json)        //response.json()是json格式化
+            this.setState({userNickName:json.NickUserName,userid:json.UserId});
+        });
+
+        //ant design里的样式
+        message.success("请求成功！");
+        this.setModalVisible(false);
 
     }
 
@@ -58,9 +81,9 @@ class PCHeader extends React.Component {
 
                 {/*空格&nbsp;*/}
                 &nbsp;&nbsp;
-                    <link target="_blank">
-                        <Button type="dashed" htmlType="button">个人中心</Button>
-                    </link>
+                <link target="_blank">
+                    <Button type="dashed" htmlType="button">个人中心</Button>
+                </link>
                 &nbsp;&nbsp;
 
                 <Button type="ghost" htmlType="button">退出</Button>
@@ -68,9 +91,8 @@ class PCHeader extends React.Component {
             </Menu.Item>
             :
             <Menu.Item key="register" class="register">
-                <Icon type="user" />注册/登录
+                <Icon type="user"/>注册/登录
             </Menu.Item>;
-
 
 
         return (
@@ -90,7 +112,8 @@ class PCHeader extends React.Component {
                     <Col span={16}>
                         {/*mode="horizontal"水平排列*/}
                         {/*selectedKeys={[this.state.current]}，默认选中高亮的 ,值初始化成了Menu.Item key="top" 的值，在上面的constructor()中 */}
-                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
+                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)}
+                              selectedKeys={[this.state.current]}>
                             <Menu.Item key="top">
                                 {/*Icon type="appstore"  设置哪种类型的小图标*/}
                                 <Icon type="appstore"/>头条
@@ -120,23 +143,28 @@ class PCHeader extends React.Component {
                         </Menu>
 
 
-
                         {/*wrapClassName是对话框外层容器的类名*/}
                         {/*okText="关闭"若无，则默认ok的button上的文字是"确定"*/}
-                        <Modal title="用户中心" wrapClassName="vertical-center-moal" visible={this.state.modalVisible} onCancel={()=>this.setModalVisible(false)}  onOk={()=>this.setModalVisible(false)} okText="关闭">
-                            <FormItem label="账户">
-                                <Input placeholder="请输入账号"{...getFieldProps('r_userName')}/>
-                            </FormItem>
-                            <FormItem label="密码">
-                                 <Input type="password" placeholder="密码"{...getFieldProps('r_password')}/>
-                            </FormItem>
-                            <FormItem label="确认密码">
-                                  <Input type="password" placeholder="确认密码"{...getFieldProps('r_confirmPassword')}/>
-                             </FormItem>
-                                <Button type="primary" htmlType="submit">注册</Button>
+                        <Modal title="用户中心" wrapClassName="vertical-center-moal" visible={this.state.modalVisible}
+                               onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)}
+                               okText="关闭">
+                            <Tabs type="card">
+                                <TabPane tab="注册" key="2">
+                                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label="账户">
+                                            <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
+                                        </FormItem>
+                                        <FormItem label="密码">
+                                            <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+                                        </FormItem>
+                                        <FormItem label="确认密码">
+                                            <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit" >注册</Button>
+                                    </Form>
+                                </TabPane>
+                            </Tabs>
                         </Modal>
-
-
 
 
                     </Col>
@@ -146,7 +174,6 @@ class PCHeader extends React.Component {
         );
     };
 }
-
 
 
 //涉及到ant desigin中的form，需要这样的语句 create一下，官网有文档例子
